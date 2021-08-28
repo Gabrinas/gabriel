f00c66d9a365a66c2de4e1f742e89180698e6900
Sobola Gabriel <sobolagabriel@gmail.com>
2021-08-28 18:54:58 +0100

    Create myname.js

---
+++ myname.js
@@ -0,0 +1,10 @@
+function myname(){
+  First_name = "Gabriel";
+  Middle_name = "Oluwatobi";
+  Last_name = "Sobola";
+  name = `My name is ${Last_name}, ${First_name} ${Middle_name}`;
+  console.log(name); 
+  }
+myname();
+
+
